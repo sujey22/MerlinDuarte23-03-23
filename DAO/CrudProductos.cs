38dbfa51d6a66b47c2f15b344747a6cf43cfafee
sujey22 <da0089032022@unab.edu.sv>
2023-03-23 12:42:55 -0600

    Creando proyecto

---
+++ DAO/CrudProductos.cs
@@ -0,0 +1,27 @@
+﻿using MerlinDuarte23_03_23.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerlinDuarte23_03_23.DAO
+{
+    public class CrudProductos
+    { 
+        public void AgregarProductos(Producto ParametroProducto)
+        {
+            using(AlmacenContext db = new AlmacenContext()) 
+            {
+                Producto Prod = new Producto();
+                
+                Prod.Nombre = ParametroProducto.Nombre;
+                Prod.Descripción = ParametroProducto.Descripción;
+                Prod.Precio = ParametroProducto.Precio;
+                Prod.Stock = ParametroProducto.Stock;
+                db.Add(Prod);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using MerlinDuarte23_03_23.DAO;
+using MerlinDuarte23_03_23.Models;
+
+CrudProductos CrudProductos = new CrudProductos();
+Producto Prod = new Producto();
+
+Console.WriteLine(@"Menú
+Pulse el numero 1 para Insertar un nuevo Producto:
+Pulse el numero 2 para Salir:");
+
+var Menú = Convert.ToInt32(Console.ReadLine());
+
+switch (Menú)
+{
+    case 1:
+        bool bucle = true;
+        while (bucle == true)
+        {
+            Console.WriteLine("Ingrese el nombre del producto: ");
+            Prod.Nombre = Console.ReadLine();
+            Console.WriteLine("\nIngrese una descripción del producto: ");
+            Prod.Descripción = Console.ReadLine();
+            Console.WriteLine("\nIngrese el precio del producto: $");
+            Prod.Precio = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("\nIngrese la cantidad de productos existentes");
+            Prod.Stock = Convert.ToInt32(Console.ReadLine());
+
+            CrudProductos.AgregarProductos(Prod);
+
+
+            Console.Write(@"
+El Producto se ingreso correctamente
+
+Pulsa 1 para seguir insertando Productos
+Pulsa 2 para salir
+: ");
+
+            var Menú2 = Convert.ToInt32(Console.ReadLine());
+
+            switch (Menú2)
+            {
+                case 1:
+                    bucle = true;
+                    break;
+
+                case 2:
+                    bucle = false;
+                    break;
+
+                default:
+                    Console.Write("Ingrese una opción del menú");
+                    break;
+            }
+        }
+        break;
+    case 2: 
+        Console.WriteLine("Gracias por utilizar nuestro programa");
+        break;
+
+    default:
+        Console.WriteLine("La opción no se encuentra disponible");
+        break;
+}
